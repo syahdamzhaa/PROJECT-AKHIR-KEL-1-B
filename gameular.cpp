503ae1d2dfe1911cc5c7ab81a65ccbcbeb95649d
felizamutiara <felizamutiara478@gmail.com>
2023-12-11 12:35:59 +0700

    aku nambahin gameover nya aja yaa

--- gameular.cpp
+++ gameular.cpp
@@ -14,6 +14,17 @@ bool game = true;
 int level = 1; // level awal agar bertambah trus
 int speed = 200; // kecepatatan awal untuk level 1
 
+void GameOverScreen() {
+    clear();
+    attron(A_BOLD);
+    attron(COLOR_PAIR(2));
+    mvprintw(maxY / 2 - 1, (maxX / 2) - 10, "GAMEE GAJEELASSSSS!");
+    attroff(COLOR_PAIR(2));
+    attroff(A_BOLD);
+    refresh();
+}
+
+
 void LoadingScreen() {
     clear();
     attron(A_BOLD);
@@ -56,77 +67,9 @@ void MengaturLevel() {//ini unutk mengatur keceptan level
 
 void Console() {
     if (kbhit()) {
-        switch (getch()) {
-            case 'a': if(arah!='d')arah = 'a'; break;
-            case 'w': if(arah!='s')arah = 'w'; break;
-            case 's': if(arah!='w')arah = 's'; break;
-            case 'd': if(arah!='a')arah = 'd'; break;
-            case 'q': game = false; break;
-        }
-    }
-}
-
-void Draw() {
-    clear();
-    WINDOW *win = newwin(maxY, maxX, 0, 0);
-    
-    refresh();
-    box(win, 0, 0);
-    wrefresh(win);
-
-    mvprintw(y, x, "o");
-    mvprintw(fruitY, fruitX, "#");
-
-    for (int i = 0; i < tailLength; i++) {
-        mvprintw(tailY[i], tailX[i], "o");
-    }
-
-    refresh();
-    Sleep(100);
-}
-
-void Algorithm() {
-    int prevX = tailX[0];
-    int prevY = tailY[0];
-    int prev2X, prev2Y;
-    tailX[0] = x;
-    tailY[0] = y;
-
-    for (int i = 1; i < tailLength; i++) {
-        prev2X = tailX[i];
-        prev2Y = tailY[i];
-        tailX[i] = prevX;
-        tailY[i] = prevY;
-        prevX = prev2X;
-        prevY = prev2Y;
-    }
-
-    if(arah=='a')x-=2;
-    if(arah=='d')x+=2;
-    if(arah=='s')y+=2;
-    if(arah=='w')y-=2;   
-
-    if (y - fruitY >= 0 && y-fruitY < 3) {//misal ular dan makanan selisih dari 3,misal di x=16 dan y=17 ,maka true //salsa
-    	if (x - fruitX >=0 && x-fruitX < 3){
-        fruitX = rand() % maxX;
-        fruitY=1;
-        score++;
-        tailLength++;
-    }
-    }
-    fruitY++;
-    
-    if(fruitY>maxY){
-    	fruitY=1;
-    	fruitX=rand() %maxX;
-	}
 
 
-    for (int i = 0; i < tailLength; i++) {
-        if (tailX[i] == x && tailY[i] == y) {
-            game = false;
-        }
-    }
+@@ -130,49 +111,47 @@
 
     
     if (x <= 0 || x >= maxX || y <= 0 || y >= maxY) {
@@ -153,6 +96,7 @@ int main() {
     
     AwalMasukGame();
 	int level = 1; // level awal
+    int level = 1; // level awal
     int speed = 200; // kecepatan awa
 
     getmaxyx(stdscr, maxY, maxX);
@@ -167,8 +111,13 @@ int main() {
         Console();
         Algorithm();
     } while (game);
+    mvprintw(maxX/2),(maxX/2)-12,"Kamu ga salah kok,gamenya salah!!")
+    
+	refresh();
+    getch();
     clear();
-    mvprintw(maxY/2,(maxX/2)-19,"Score anda adalah %d",score);
+    GameOverScreen();
+    mvprintw(maxY/2,(maxX/2)-19,"Score Kamu adalah : %d",score);
     refresh();
     getch();
     endwin();
