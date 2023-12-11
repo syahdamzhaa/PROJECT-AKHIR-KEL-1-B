6ec86160be759fd86c04769be2a27dbe6d675577
syahdamzhaa <syahdamrizky06@gmail.com>
2023-12-11 16:55:55 +0700

    Update gameular.cpp

--- gameular.cpp
+++ gameular.cpp
@@ -3,16 +3,31 @@
 #include <windows.h>  
 #include <ncurses/ncurses.h>
 
-int maxY, maxX;
-int x, y;
-char arah = 'd';
-int score = 0;
-int tailLength = 1;
-int tailX[100], tailY[100];
-int fruitX, fruitY;
+using namespace std;
+
+int maxX,maxY,x,y;
 bool game = true;
+char arah = 'w';
+int score = 0;
+int fruitX,fruitY;
+int tailLength = 1; 
+int tailX[100],tailY[100];
 int level = 1; // level awal agar bertambah trus
 int speed = 200; // kecepatatan awal untuk level 1
+
+
+
+void LoadingScreen() {
+    clear();
+    attron(A_BOLD);
+    mvprintw(maxY / 2, (maxX / 2) - 11, "Loading ya sayangg<3...");
+    attroff(A_BOLD);
+    refresh();
+    
+    // Tambahkan delay untuk simulasi loading
+    // Anda bisa menambahkan animasi loading yang lebih kompleks jika diperlukan
+    Sleep(2000); // Contoh: Delay selama 2 detik (untuk simulasi)
+}
 
 void GameOverScreen() {
     clear();
@@ -24,24 +39,9 @@ void GameOverScreen() {
     refresh();
 }
 
-
-void LoadingScreen() {
-    clear();
-    attron(A_BOLD);
-    mvprintw(maxY / 2, (maxX / 2) - 11, "Loading ya sayangg<3...");
-    attroff(A_BOLD);
-    refresh();
-
-    Sleep(3000); // ini untuk delay loadingnya 3 detik 
-}
-
-void AwalMasukGame() {
-    clear();
-    attron(A_BOLD); // ini untuk aktifin teks tebal
-    mvprintw(maxY / 2 - 2, (maxX / 2) - 10, "Selamat Datang di Game 'ULAR SI MATA DUITAN'");
-    attroff(A_BOLD); // ini non aktif teks tebal
-    mvprintw(maxY / 2, (maxX / 2) - 20, "Tekan 'S' untuk memulai permainan.");
-    mvprintw(maxY - 2, 2, "Tekan 'X' untuk keluar.");
+void NampilinSkorBaris() {
+    mvprintw(1, 2, "Skor kamu sayang: %d", score); // ini ni unutk nampilin skor di posisi baris 1, kolom 2
+    mvprintw(2, 2, "Level: %d", level); // namipilin level baris 2, kolom 2
     refresh();
 }
 
@@ -64,19 +64,126 @@ void MengaturLevel() {//ini unutk mengatur keceptan level
         }
     }
 
+void AwalMasukGame() {
+    clear();
+    attron(A_BOLD); // Mengaktifkan teks tebal
+    mvprintw(maxY / 2 - 2, (maxX / 2) - 10, "Selamat Datang di Game 'ULAR SI MATA DUITAN'");
+    attroff(A_BOLD); // Menonaktifkan teks tebal
+    mvprintw(maxY / 2, (maxX / 2) - 20, "Tekan 'S' untuk memulai permainan.");
+    mvprintw(maxY - 2, 2, "Tekan 'X' untuk keluar.");
+    refresh();
+}
 
-void Console() {
-    if (kbhit()) {
+void StartGame() {
+	LoadingScreen();
+    clear(); // Membersihkan layar sebelum memulai permainan
+    attron(A_BOLD); // Mengaktifkan teks tebal
+    attron(COLOR_PAIR(1)); // Mengatur warna teks
+    mvprintw(maxY / 2, (maxX / 2) - 24, "Tekan tombol apa aja untuk mulai gamenya sayang<3");
+    attroff(COLOR_PAIR(1)); // Menonaktifkan warna teks
+    attroff(A_BOLD); // Menonaktifkan teks tebal
+    refresh();
+    getch();
+}
 
+void About() {
+    clear();
+    attron(A_BOLD);
+    attron(COLOR_PAIR(2));
+    mvprintw(maxY / 2 - 2, (maxX / 2) - 18, "'Catch the #' adalah game ular sederhana ");
+    mvprintw(maxY / 2 - 1, (maxX / 2) - 18, "'d' Agar ular ke kanan");
+    mvprintw(maxY / 2 , (maxX / 2) - 18, "'a' Agar ular ke kiri");
+    mvprintw(maxY / 2 + 1, (maxX / 2) - 18, "'w' Agar ular ke atas");
+    mvprintw(maxY / 2 + 2, (maxX / 2) - 18, "'s' Agar ular ke bawah");
+    mvprintw(maxY / 2 + 9, (maxX / 2) + 32, "Dibuat Oleh:");
+    mvprintw(maxY / 2 + 10, (maxX / 2) + 32, "1.Feliza Mutira");
+    mvprintw(maxY / 2 + 11, (maxX / 2) +32 , "2.Lifi Anasywa");
+    mvprintw(maxY / 2 + 12, (maxX / 2) + 32, "3.Salsha Nabila");
+    mvprintw(maxY /2 + 13, (maxX / 2) + 32, "4.Syahdam Rizky Firdaus");
+    mvprintw(maxY - 2, 2, "Tekan tombol apapun untuk kembali ke menu.");
+    attroff(COLOR_PAIR(2));
+    attroff(A_BOLD);
+    refresh();
+    getch();
+    clear();
+}
+void ExitGame() {
+    endwin();
+    exit(0);
+}
 
-@@ -130,49 +111,47 @@
-
+void Draw(){
+    WINDOW *win = newwin(maxY, maxX, 0, 0);
+    refresh();
+    box(win, 0, 0);
+    wrefresh(win);
+    mvprintw(y,x,"@");
+    mvprintw(fruitY,fruitX,"<3");
+    NampilinSkorBaris(); // Menampilkan skor dan status permainan
+       
     
-    if (x <= 0 || x >= maxX || y <= 0 || y >= maxY) {
-        game = false;
+    for(int i=0;i<tailLength;i++){
+        mvprintw(tailY[i],tailX[i],"o");
+    }
+    refresh();
+    Sleep(speed);
+}
+
+void Console(){
+    if(kbhit()){
+        switch(getch()){
+            case 'a' : if(arah!='d')arah = 'a';break;
+            case 'd' : if(arah!='a')arah = 'd';break;
+            case 'w' : if(arah!='s')arah = 'w';break;
+            case 's' : if(arah!='w')arah = 's';break;
+            case 'q' : game = false; break;
+        }
     }
 }
-  if (score >= 3 && level == 1) { // contoh: naik level saat skor mencapai 6 pada level 1
+
+void Algorithm(){
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prevX2,prevY2;
+    tailX[0] = x;
+    tailY[0] = y;
+
+    for(int i=1;i<tailLength;i++){
+        prevX2 = tailX[i];
+        prevY2 = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prevX2;
+        prevY = prevY2;
+    }
+
+    if(arah=='a')x-=2;
+    if(arah=='d')x+=2;
+    if(arah=='s')y+=2;
+    if(arah=='w')y-=2;   
+
+    if (y - fruitY >= 0 && y-fruitY < 3) {//misal ular dan makanan selisih dari 3,misal di x=16 dan y=17 ,maka true //salsa
+    	if (x - fruitX >=0 && x-fruitX < 3){
+        fruitX = rand() % maxX;
+        fruitY=1;
+        score++;
+        tailLength++;
+    }
+    }
+    	fruitY++;
+    	
+    if(fruitY>maxY){
+    	fruitY=1;
+    	fruitX=rand() %maxX;
+	}
+    if(x <= 0 || y <=0 || x>=maxX || y>=maxY){
+        game = false;
+    }
+    for(int i=0;i<tailLength;i++){
+        if(x==tailX[i]&&y==tailY[i])game=false;
+    }
+    
+    if (score >= 3 && level == 1) { // contoh: naik level saat skor mencapai 6 pada level 1
         level = 2; 
 		}
     if(score >= 6 && level == 2){
@@ -88,39 +195,69 @@ void Console() {
         MengaturLevel(); // Mengatur kecepatan sesuai level yang baru
 }
 
-int main() {
-    // Initialize ncurses
+int main(){
     initscr();
-    noecho();  // Don't display typed characters
-    curs_set(0);  // Hide the cursor
-    
-    AwalMasukGame();
-	int level = 1; // level awal
-    int level = 1; // level awal
-    int speed = 200; // kecepatan awa
+    start_color(); // Mengaktifkan penggunaan warna
+    init_pair(1, COLOR_CYAN, COLOR_BLACK); // Pasangan warna untuk Start
+    init_pair(2, COLOR_GREEN, COLOR_BLACK); // Pasangan warna untuk About
 
+    noecho();
+    curs_set(0);
     getmaxyx(stdscr, maxY, maxX);
-
-    y = maxY / 2;
-    x = maxX / 2;
+    x = maxX / 2, y = maxY / 2;
     fruitX = rand() % maxX;
     fruitY = 1;
-
+    
+    AwalMasukGame();
+    int level = 1; // level awal
+    int speed = 200; // kecepatan awal
+    int userInput;
     do {
+        clear();
+        mvprintw(maxY / 2 - 4, (maxX / 2)-18, "Welcome to the game 'Ular si mata buaya'");
+        attron(COLOR_PAIR(1)); // Mengaktifkan warna teks
+        mvprintw(maxY / 2 - 1, (maxX / 2) - 12, "Press 'S' to Start");
+        attroff(COLOR_PAIR(1)); // Menonaktifkan warna teks
+        attron(COLOR_PAIR(2));
+        mvprintw(maxY / 2 , (maxX / 2) - 12, "Press 'A' for About");
+        attroff(COLOR_PAIR(2));
+        mvprintw(maxY / 2 + 1, (maxX / 2) - 12, "Press 'X' to Exit");
+        refresh();
+
+        userInput = getch();
+        switch (userInput) {
+            case 's':
+            case 'S':
+                StartGame();
+                break;
+            case 'a':
+            case 'A':
+                About();
+                break;
+            case 'x':
+            case 'X':
+                ExitGame();
+                break;
+            default:
+                break;
+        }
+    } while (userInput != 's' && userInput != 'S');
+
+    // Mulai permainan jika tidak keluar dari loop
+    clear(); // Membersihkan layar sebelum memulai permainan
+    do{
         Draw();
         Console();
         Algorithm();
-    } while (game);
-    mvprintw(maxX/2),(maxX/2)-12,"Kamu ga salah kok,gamenya salah!!")
-    
-	refresh();
+    }while(game);//BAGIAN FELIZA
+    mvprintw(maxY/2,(maxX/2)-12,"Kamu ga salah kok,gamenya salah!!");
+   
+    refresh();
     getch();
     clear();
     GameOverScreen();
-    mvprintw(maxY/2,(maxX/2)-19,"Score Kamu adalah : %d",score);
+    mvprintw(maxY/2-2,(maxX/2)-11,"Score Kamu adalah : %d",score);
     refresh();
     getch();
-    endwin();
-
-    return 0;
+	endwin();
 }
